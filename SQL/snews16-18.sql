66638b7f09d9799cb5c6c29c7f0c2a08de235c9c
bambsant <159030124+bambsant@users.noreply.github.com>
2024-02-05 21:49:01 +0700

    Add files via upload

---
+++ SQL/snews16-18.sql
@@ -0,0 +1,58 @@
+--
+--  Use this file to update an existing sNews 1.6 database only.
+--  Last Updated: July 14, 2016
+-- -----------------------------------------------------------------------------
+
+-- DON'T FORGET TO CHANGE DATABASE NAME IN THIS CASE I WILL USE snews18
+ALTER DATABASE `snews18` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+SET NAMES 'utf8';
+
+SET storage_engine = INNODB;
+
+
+-- ---- Alter Articles table:
+	ALTER TABLE `articles`
+ADD (	`extraid` varchar(8) default NULL,
+	`page_extra` varchar(8) default NULL,
+	`show_on_home` enum('YES','NO') default 'YES',
+	`show_in_subcats` enum('YES','NO') default 'NO',
+	`artorder` smallint(6) NOT NULL default '0',
+	`visible` varchar(6) default 'YES',
+	`default_page` varchar(6) default 'NO'
+	);
+
+-- ---- Alter Categories table:
+	ALTER TABLE `categories`
+	ADD (`subcat` int(8) NOT NULL default '0' );
+
+-- ---- Insert default category into Categories table:
+	INSERT INTO `categories` VALUES ('1', 'Uncategorized', 'uncategorized', '', 'YES', '1', '0');
+
+-- ---- Create new Extras table:
+	CREATE TABLE `extras` (
+	`id` int(8) primary key auto_increment,
+	`name` varchar(40) NOT NULL,
+	`seftitle` varchar(100) default NULL,
+	`description` varchar(100) NOT NULL
+);
+
+-- ---- Insert data into Extras table:
+	INSERT INTO `extras` VALUES (1, 'Extra', 'extra', 'The default extra');
+
+-- ---- Alter Settings Table:
+        ALTER TABLE `settings`
+ 	MODIFY `value` varchar(255) not null;
+	INSERT INTO `settings` VALUES ('', 'show_cat_names', '');
+	INSERT INTO `settings` VALUES ('', 'mail_on_comments', '');
+	INSERT INTO `settings` VALUES ('', 'comment_repost_timer', '20');
+	INSERT INTO `settings` VALUES ('', 'enable_comments', 'NO');
+	INSERT INTO `settings` VALUES ('', 'enable_extras', 'NO');
+	INSERT INTO `settings` VALUES ('', 'last_date', NOW());
+	INSERT INTO `settings` VALUES ('', 'file_extensions', 'phps,php,txt,inc,htm,html');
+	INSERT INTO `settings` VALUES ('', 'allowed_files', 'php,htm,html,txt,inc,css,js,swf');
+	INSERT INTO `settings` VALUES ('', 'allowed_images', 'gif,jpg,jpeg,png');
+
+-- ---- ADD SPECIAL KEYS to Articles and Comments tables:
+	ALTER TABLE `articles` ADD INDEX ( `show_on_home` );
+	ALTER TABLE `comments` ADD INDEX ( `articleid` );
